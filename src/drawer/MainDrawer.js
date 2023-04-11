d18c0e01b6ddb9985e798c4765b0ab27f22f7919
mby444 <bimayudha161@gmail.com>
2023-04-11 19:43:26 +0700

    Add empty drawer

--- src/drawer/MainDrawer.js
+++ src/drawer/MainDrawer.js
@@ -1,14 +1,27 @@
+import { Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createDrawerNavigator, DrawerContentScrollView } from "@react-navigation/drawer";
 import HomeStack from "../stack/HomeStack";
 import Watchlist from "../screen/Watchlist";
 
 const Drawer = createDrawerNavigator();
 
+function MainDrawerContent() {
+  return (
+    <DrawerContentScrollView>
+      
+    </DrawerContentScrollView>
+  );
+}
+
 export default function MainDrawer() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="HomeStack" screenOptions={{ headerShown: false }}>
+      <Drawer.Navigator
+        initialRouteName="HomeStack"
+        drawerContent={MainDrawerContent}
+        screenOptions={{ headerShown: false }}
+      >
         <Drawer.Screen name="HomeStack" component={HomeStack} />
         <Drawer.Screen name="Watchlist" component={Watchlist} />
       </Drawer.Navigator>
